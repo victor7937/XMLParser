86295d070eef9c92cf5d28bddb75b228dd56c397
Victor Vyrostak <virostak@mail.ru>
2021-02-16 14:00:10 +0300

    fix FileReader

--- src/main/java/by/victor/jwd/dao/impl/XMLDAOImpl.java
+++ src/main/java/by/victor/jwd/dao/impl/XMLDAOImpl.java
@@ -6,28 +6,16 @@ import by.victor.jwd.parser.ParserFactory;
 import by.victor.jwd.parser.XMLParser;
 import by.victor.jwd.tree.XMLTree;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
 
 public final class XMLDAOImpl implements XMLDAO {
 
     private static final String FILENAME = "sample.xml";
-    private boolean fileNotOpenFlag;
 
     @Override
     public XMLTree loadXMLTree() {
         ParserFactory factory = ParserFactory.getInstance();
         XMLParser xmlParser = factory.getXMLParser();
-        String xmlStringForm = "";
-        try {
-            xmlStringForm = XmlFileReader.loadXMLFileStringForm(FILENAME);
-        } catch (IOException | URISyntaxException e) {
-            fileNotOpenFlag = true;
-        }
-        if (fileNotOpenFlag) {
-            return null;
-        }
-
-        return xmlParser.constructTree(xmlStringForm);
+        String xmlStringForm = XmlFileReader.loadXMLFileStringForm(FILENAME);
+        return xmlStringForm.isEmpty() ? null : xmlParser.constructTree(xmlStringForm);
     }
 }
--- src/main/java/by/victor/jwd/dao/utils/XmlFileReader.java
+++ src/main/java/by/victor/jwd/dao/utils/XmlFileReader.java
@@ -2,18 +2,30 @@ package by.victor.jwd.dao.utils;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
 
 public final class XmlFileReader {
 
-    public static String loadXMLFileStringForm(String fileName) throws IOException, URISyntaxException {
+    public static String loadXMLFileStringForm(String fileName) {
+        boolean fileErrFlag = false;
         StringBuilder xmlStringForm = new StringBuilder();
-        try(Stream<String> lines = Files.lines(Paths.get(ClassLoader.getSystemResource(fileName).toURI()))){
-            lines.forEach(xmlStringForm::append);
+        Stream<String> lines = null;
+        try {
+            URL url = ClassLoader.getSystemResource(fileName);
+            if (url != null) {
+                lines = Files.lines(Paths.get(url.toURI()));
+                lines.forEach(xmlStringForm::append);
+            }
+        } catch ( URISyntaxException | IOException e) {
+            fileErrFlag = true;
+        } finally {
+            if (lines != null) {
+                lines.close();
+            }
         }
-        return xmlStringForm.toString();
+        return fileErrFlag ? "" : xmlStringForm.toString();
     }
-
 }
